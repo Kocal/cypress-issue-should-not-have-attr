9947ed8d67ffe9877d275787d1b167f8544535e5
Hugo Alliaume <kocal@live.fr>
2019-04-09 09:39:54 +0200

    Reproduce the issue

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,23 @@
 describe('page', () => {
-  it('works', () => {})
-})
+  before(() => {
+    cy.visit('/index.html');
+  });
+
+  it('works', () => {
+    // For the first header, I'm testing if it has the attribute `data-sorted` with the good value, and if the arrow is good
+    cy.get('[data-testid=datatable-header-sorted]')
+      .should('have.class', 'sortable')
+      .should('have.attr', 'data-sorted', 'desc')
+      .find('> i')
+      .should('have.class', 'fa-sort-down');
+  });
+
+  it('not works', () => {
+    // For the second header, I'm testing if it has NOT the attribute `data-sorted`, but I can't test the arrow
+    cy.get('[data-testid=datatable-header-sortable]')
+      .should('have.class', 'sortable')
+      .should('not.have.attr', 'data-sorted')
+      .find('> i')
+      .should('have.class', 'fa-sort');
+  });
+});
